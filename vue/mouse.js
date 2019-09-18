5fd3678602918a64b47a184ff7dd4250fbe0c577
danieç <dqrtec@hotmail.com>
2019-09-18 17:18:36 -0300

    Update mouse.js

--- vue/mouse.js
+++ vue/mouse.js
@@ -5,8 +5,8 @@
 </head>
 <body>
 <div id="app">
-	<input type="range"  min="0" max="300" v-model="x">
-	<input type="range"  min="0" max="300" v-model="y">
+	<input type="range"  min="0" max="200" v-model="x">
+	<input type="range"  min="0" max="200" v-model="y">
 	<canvas id="tela" width="200" height="200" style="border:1px solid #c3c3c3;"></canvas>
 	{{x}} {{y}} {{ alertar() }}
 	
@@ -28,10 +28,12 @@
 		function desenhar(app) {
 			var canvas = document.getElementById("tela");
 			var ctx = canvas.getContext("2d");
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			ctx.fillStyle = "#FF0000";
 			var x = app.x
 			var y = app.y
-			ctx.fillRect(0,0,x,y);
+			ctx.fillRect(0,y,canvas.width,15);
+			ctx.fillRect(x,0,15,canvas.height);
 		}
 
 	</script>
